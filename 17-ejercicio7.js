e1b5eefa0f6177516853f8286183f3ad3a0f7d26
miguel <usmo300@hotmail.com>
2021-05-03 09:48:30 +0200

    Subidos los últimos ejercicios

--- 17-ejercicio7.js
+++ 17-ejercicio7.js
@@ -1,3 +1,15 @@
 /*
 Tabla de multiplicar de un número introducido por pantalla
 */
+
+var multNum = Number(prompt('¿Cual es la tabla de multiplicar que desea?'));
+var multiplicador = 0;
+
+document.write(`Tabla de multiplicar del número ${multNum} <br>`)
+while (multiplicador != 11){
+    var multiplicacion = multNum * multiplicador;
+
+    document.write(`${multNum} X ${multiplicador} = ${multiplicacion} <br>`);
+
+    multiplicador++;
+}
--- 18-ejercicio8.js
+++ 18-ejercicio8.js
@@ -6,3 +6,20 @@ Si metemos uno mal que nos los vuelva a pedir.
 En el cuerpo de la página y por la consola el resultado
 de sumar, restar, multiplicar y dividir esas dos cifras.
 */
+
+do{
+    var n1 = Number(prompt('inserte el primer número'));
+    
+}while (isNaN(n1));
+
+do{
+    var n2 = Number(prompt('inserte el segundo número'));
+
+}while (isNaN(n2));
+
+var suma = n1 + n2;
+var resta = n1 - n2;
+var multiplicacion = n1 * n2;
+var division = n1 / n2;
+
+document.write(`Suma=${suma}<br>Resta=${resta}<br>Multiplicación=${multiplicacion}<br>División=${division}<br>`)
